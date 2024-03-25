2b9d3af80bd3e67a6ff485ea02ed6b88bf43ad57
James <145038163+SteelSinter@users.noreply.github.com>
2024-03-24 21:32:43 -0600

    Add files via upload

--- src/application/EnemyWave.java
+++ src/application/EnemyWave.java
@@ -1,16 +1,18 @@
 package application;
 
 public class EnemyWave {
-	private Game game;
-	private Class<Enemy> type;
-	private boolean mustKillAll; // If the player needs to kill all of them to progress
+	private enum EnemyType {RIGHTTOLEFT, STAY, SPIN}
 	public enum Formation {SQUARE, SINE, TRIANGLE}
-	private Formation formation;
-	private int count;
 	private double startX = Main.scene.getWidth();
 	private double startY = Main.scene.getHeight() / 4;
+	private boolean mustKillAll; // If the player needs to kill all of them to progress
+	private Formation formation;
+	private EnemyType type;
+	private Game game;
+	private int count;
 	
-	EnemyWave(int count, Class<Enemy> type, Formation formation) {
+	
+	EnemyWave(int count, EnemyType type, Formation formation) {
 		game = Main.getGame();
 		this.count = count;
 		this.formation = formation;
@@ -41,7 +43,11 @@ public class EnemyWave {
 		System.out.println("Creating square wave of " + count + " enemies");
 		double x = startX, y = startY;
 		for (int i = 0; i < count; i++) {
-			Main.getGame().addObject(new  (GameObject) type, x, y);
+			switch (type) {
+			case RIGHTTOLEFT:
+				game.addObject(new RightToLeft(), x, y);
+				break;
+			}
 			if (i % 5 == 0 && i != 0) {
 				x += 50;
 				y = startY;
